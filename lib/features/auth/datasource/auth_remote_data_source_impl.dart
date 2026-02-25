2bd1ee74c7d0685013d0d4c9a6379b8d0131e455
Vishal Dhanawade <vishal@Vishals-MacBook-Air.local>
2026-02-25 19:35:04 +0530

    Datasource Issue Fix

---
+++ lib/features/auth/datasource/auth_remote_data_source_impl.dart
@@ -0,0 +1,26 @@
+import 'package:splitzy/features/auth/data/model/user_model.dart'
+    show UserModel;
+import 'package:splitzy/features/auth/datasource/auth_remote_data_source.dart';
+import 'package:splitzy/features/auth/domain/usecases/sign_in_with_credentials_usecase.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' as supa;
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final supa.SupabaseClient supabase;
+  const AuthRemoteDataSourceImpl(this.supabase);
+
+  @override
+  Future<UserModel> signUpWithCredentials({
+    required SignUpParams params,
+  }) async {
+    try {
+      final result = await supabase.auth.signUp(
+        email: params.email,
+        password: params.password,
+        data: {'name': params.name},
+      );
+      return UserModel.fromjson(result.user!.toJson());
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+}
