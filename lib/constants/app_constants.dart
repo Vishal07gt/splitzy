cf90cb041f260af12c5206d200d50654571a351c
Vishal Dhanawade <vishal@Vishals-MacBook-Air.local>
2026-02-26 00:05:20 +0530

    App Constants

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,5 @@
 
 class AppConstants {
-  static const SUPABASE_URL = '';
-  static const SUPABASE_ANON_KEY = '';
+  static const SUPABASE_URL = 'https://xzoupptvzbztyfaplnta.supabase.co';
+  static const SUPABASE_ANON_KEY = 'sb_publishable_gd4JwA2eb8COI-hWwkKdFQ_xkIo6lHz';
 }
